e8e9447397f04cfa9173309c7df8108ee7cdb50b
Tejas Ahluwalia <tejas@appsmith.com>
2022-11-07 16:57:37 +0530

    Responsive youtube embed component

---
+++ website/src/components/YoutubeEmbed.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+
+export default function YoutubeEmbed(props) {
+    const { title, videoId, caption, bgColor } = props;
+    return (
+      <div style={{ backgroundColor: bgColor ? bgColor : 'transparent' }}>
+        <figure style={{
+          width: '100%',
+          margin: '0',
+        }}>
+          <iframe
+            style={{
+              width: '100%',
+              height: 'auto',
+              aspectRatio: '16/9'
+            }}
+            src={`https://youtube-nocookie.com/embed/${videoId}`}
+            srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://youtube.com/embed/${videoId}><img src=https://img.youtube.com/vi/${videoId}/maxresdefault.jpg alt='${title}'><span>▶</span></a>`}
+            frameBorder="0"
+            allowFullScreen
+            title={title}
+            loading="lazy"
+          ></iframe>
+          <figcaption>{caption}</figcaption>
+        </figure>
+      </div>
+    );
+  };
