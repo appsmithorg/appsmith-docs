df040b28baca3f86e06e16a8df1653d58adb8b3e
Tejas Ahluwalia <tejas@appsmith.com>
2022-11-07 19:52:20 +0530

    Adjust shadow for play icon

--- website/src/components/YoutubeEmbed.js
+++ website/src/components/YoutubeEmbed.js
@@ -13,8 +13,8 @@ export default function YoutubeEmbed(props) {
               height: 'auto',
               aspectRatio: '16/9'
             }}
-            src={`https://youtube-nocookie.com/embed/${videoId}`}
-            srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://youtube.com/embed/${videoId}><img src=https://img.youtube.com/vi/${videoId}/maxresdefault.jpg alt='${title}'><span>▶</span></a>`}
+            src={`https://youtube.com/embed/${videoId}`}
+            srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.25em gray}</style><a href=https://youtube.com/embed/${videoId}><img src=https://img.youtube.com/vi/${videoId}/maxresdefault.jpg alt='${title}'><span>▶</span></a>`}
             frameBorder="0"
             allowFullScreen
             title={title ? title : 'YouTube video player'}
