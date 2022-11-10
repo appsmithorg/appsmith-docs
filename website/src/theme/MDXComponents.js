48d4ed0a5f5241fbbca92ddc9013b3940f1722ee
Tejas Ahluwalia <tejas@appsmith.com>
2022-11-10 16:20:17 +0530

    Create generic VideoEmbed component

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -1,10 +1,10 @@
 import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
-import YoutubeEmbed from '@site/src/components/YoutubeEmbed';
+import VideoEmbed from '@site/src/components/VideoEmbed';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
-  YoutubeEmbed,
+  VideoEmbed,
 };
