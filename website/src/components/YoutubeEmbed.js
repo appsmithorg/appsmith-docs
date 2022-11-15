741f380ca76ad64f63d1c4e412ce494e4f6e2639
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2022-11-15 19:17:10 +0530

    fix: added video caption styles
    to centre align the captions and have font style as italic.

--- website/src/components/YoutubeEmbed.js
+++ website/src/components/YoutubeEmbed.js
@@ -22,7 +22,7 @@ export default function YoutubeEmbed(props) {
             title={title ? title : 'YouTube video player'}
             loading="lazy"
           ></iframe>
-          { caption && <figcaption>{caption}</figcaption> }
+          { caption && <figcaption align="center"><i>{caption}</i></figcaption> }
         </figure>
     );
   };
