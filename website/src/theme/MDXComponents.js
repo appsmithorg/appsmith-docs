827f8f1a1740ba004c7dd265fce4f350fb8ceed1
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2022-12-22 14:31:58 +0530

    removed message component changes
    Merged it to Main as part of #698

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -2,11 +2,9 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 import VideoEmbed from '@site/src/components/VideoEmbed';
-import Message from '@site/src/components/Message';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
   VideoEmbed,
-  Message,
 };
