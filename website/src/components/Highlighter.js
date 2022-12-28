b7b402b5343a9fa73c8218237da407485ac54642
Goutham Pratapa <pratapagoutham@gmail.com>
2022-12-28 13:29:57 +0530

    feat: Migrate to Community Edition Appsmith to Helm Chart v2 (#611)
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>

--- website/src/components/Highlighter.js
+++ website/src/components/Highlighter.js
@@ -1,18 +1,16 @@
 import React from "react";
 /**
  * The component can be used to highlight the text in the documentation. 
- * For example, use it to highlight the errors, warnings in the troubleshooting guide.
+ * For example, use it to highlight the command output, warnings in the troubleshooting guide.
  * @param {*} props: 
- *                  messageContainerClassName - is the css class name that you want to style the component. 
- *                  caption - message caption for example, error, warning etc.
- *                  captionClassName - the css class name to style the heading
- *                  messageContent - the content that you wish to display in the component
- *                  messageContentClassName - the css class name to style the message content
+ *                  highlighterClassName - is the css class name that you want to style the component. 
+ *                  content - the content that you wish to display in the component
+ *                  htmlContent - the content that contains html tags
  * @returns a html component
  */
 export default function Highlighter(props) {
-    const {highlighterClassName, content} = props;
+    const {highlighterClassName, content, htmlContent} = props;
     return (
-        <span className={highlighterClassName}>{content}</span>
+        <div className={highlighterClassName} dangerouslySetInnerHTML={{__html: htmlContent}}>{content}</div>
     );
 };
