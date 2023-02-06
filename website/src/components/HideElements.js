0cd39ec3033c32fc1b1f131a30c3d97b256ddcaa
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2023-02-06 14:24:33 +0530

    feat: Add shorter version of Docker and Kubernetes installation (#757)
    - Added a component for hiding the footer, top bar, and edit page link
    for selected pages. - Done
    - Added initial draft for docker business edition installation - Initial
    Review
    TO - DO
    - Add a shorter version for Kubernetes installation
    - Add a shorter version for Docker Upgrade to Business Edition
    - Add a shorter version for Kubernetes Upgrade to Business Edition
    - Add a new `docker-compose.yaml` file with BE image 
    - Create bitly link for the `docker-compose.yaml` file.
    
    ---------
    
    Signed-off-by: Jingpiao Chen <chenjingpiao@gmail.com>
    Co-authored-by: Pranay105 <pranaybhardwaj@Pranays-MacBook-Pro.local>
    Co-authored-by: Dancia <32227135+danciaclara@users.noreply.github.com>
    Co-authored-by: danciaclara <claradancia20@gmail.com>
    Co-authored-by: Akhil Varma <16632903+infinitetrooper@users.noreply.github.com>
    Co-authored-by: Filipe Medeiros <hello@filipesm.eu>
    Co-authored-by: Chen Jingpiao <chenjingpiao@gmail.com>
    Co-authored-by: Collin Sparks <51402465+spark-c@users.noreply.github.com>
    Co-authored-by: Harshil Patel <43263619+harshilp24@users.noreply.github.com>
    Co-authored-by: Dhruvik Neharia <dhruvik@appsmith.com>
    Co-authored-by: Pranay <48308728+Pranay105@users.noreply.github.com>
    Co-authored-by: harshilp24 <harshilpatel9912@gmail.com>
    Co-authored-by: Kiara Sta Maria <112195964+allysm@users.noreply.github.com>
    Co-authored-by: Sumesh Pradhan <sumesh@appsmith.com>
    Co-authored-by: Souma Ghosh <103924539+souma-ghosh@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Shrikant Sharat Kandula <shrikant@appsmith.com>
    Co-authored-by: Arpit Mohan <mohanarpit@users.noreply.github.com>
    Co-authored-by: dilippitchika <dilip@appsmith.com>
    Co-authored-by: Collin Sparks <cklsparks@gmail.com>
    Co-authored-by: Bharath Natarajan <bharath31@users.noreply.github.com>
    Co-authored-by: Confidence Okoghenun <cokoghenun@gmail.com>

---
+++ website/src/components/HideElements.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+
+export default function Message(props) {
+    hideFooter();
+    hideTopBar();
+    hideEditPage();
+
+return true;
+}
+
+
+function hideFooter() {
+    if (ExecutionEnvironment.canUseDOM){
+        Array.prototype.forEach.call(document.getElementsByClassName('footer'), function(element) {
+            element.style.display = 'none';
+        })
+    }
+}
+
+
+function hideTopBar() {
+    if (ExecutionEnvironment.canUseDOM){
+        Array.prototype.forEach.call(document.getElementsByClassName('navbar--fixed-top'), function(element) {
+            element.style.display = 'none';
+        })
+    }
+        
+}
+
+function hideEditPage() {
+    if (ExecutionEnvironment.canUseDOM){
+        Array.prototype.forEach.call(document.getElementsByClassName('theme-doc-footer-edit-meta-row row'), function(element) {
+             element.style.display = 'none';
+         })
+    }
+        
+}
