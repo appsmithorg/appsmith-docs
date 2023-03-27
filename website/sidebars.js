de4ca08056247ada2a60aa314c54d20783e5ac21
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2023-03-27 15:05:34 +0530

    fix: Rehaul docker installation guide (#1031)
    - Updates to Docker Installation Page
       - Installation instructions for Docker Compose
    - Added a new doc for Updating Appsmith
       - The doc is added under Instance Management
       - The doc instructs users to update Appsmith to the latest version
    - Further reading removed - as users already have Appsmith instance up
    and running and configured as per their needs. This page only instructs
    them to update to the latest version.
    - Restart containers section removed - as this is obsolete
    
    ---------
    
    Co-authored-by: Dancia <32227135+danciaclara@users.noreply.github.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -165,6 +165,7 @@ const sidebars = {
               items: [
                 'getting-started/setup/instance-management/supervisor',
                 'getting-started/setup/instance-management/appsmithctl',
+                'getting-started/setup/instance-management/update-appsmith',
                 'getting-started/setup/instance-management/maintenance-window',
               ],
             },
