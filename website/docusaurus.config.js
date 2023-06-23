66df4e93bccfde35fcb1e33f854459c830e4be74
Tejas Ahluwalia <tejas@appsmith.com>
2023-06-23 19:40:54 +0530

    feat: add new color scheme

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
 
   presets: [
     [
-      'classic',
+      '@docusaurus/preset-classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
        docs: {
