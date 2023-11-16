093ed137e16293dd65b799e1a373b84d955fb038
Nikhil <nikhil.nandagopal@gmail.com>
2023-11-16 14:32:07 +0530

    Added guide for air-gapping and fixed telemetry

--- website/sidebars.js
+++ website/sidebars.js
@@ -89,6 +89,7 @@ const sidebars = {
                 'getting-started/setup/installation-guides/digitalocean',
                 'getting-started/setup/installation-guides/heroku',
                 'getting-started/setup/installation-guides/ansible',
+                'getting-started/setup/installation-guides/air-gapped',
               ],
             },
             {
