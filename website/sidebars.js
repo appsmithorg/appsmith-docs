c860af0212ad6d61a8cc8aafca0aaff047b61b26
rashmi-sahoo-git <144310336+rashmi-sahoo-git@users.noreply.github.com>
2023-11-30 18:07:24 +0530

    fix: Insert data guide (#1900)
    closes #1901 
    ## Checklist
    I have:
    - [ ] run the content through Grammarly
    - [ ] linked to sample apps when relevant
    - [ ] added the meta description for each page in the PR
    - [ ] minimized the callouts and added only when necessary
    - [ ] added the `queryString` parameter to the Tabs (if used)
    - [ ] masked PII in images. For example, login credentials, account
    details, and more
    - [ ] added images only when necessary
    - [ ] deleted the images that are no longer used for the updated pages
    in the PR
    - [ ] followed the image file naming convention while renaming or adding
    new images. (Use lowercase letters, dashes between words, and be as
    descriptive as possible)
    - [ ] used the `<figure/>` tag instead of a markdown representation for
    images
    - [ ] added the `<figcaption/>` tag to add a caption to the image
    - [ ] added the `alt` attribute in the `<img/>` tag
    - [ ] verified and updated the cross-references or created redirect
    rules for the changed or removed content
    - [ ] reviewed and applied the style changes for UI formatting. For
    example, Bold the UI elements(Buttons on screen) used in the doc.
    
    ---------
    
    Co-authored-by: Dancia <32227135+danciaclara@users.noreply.github.com>

--- website/sidebars.js
+++ website/sidebars.js
@@ -344,10 +344,11 @@ const sidebars = {
                 'build-apps/how-to-guides/search-and-filter-table-data'
               ],
             },
+            'build-apps/how-to-guides/insert-data',
+            'build-apps/how-to-guides/submit-form-data',
             'reference/widgets/table/inline-editing',
             'build-apps/how-to-guides/refresh-table-data',
             'build-apps/how-to-guides/Upload-CSV-Data-to-Table',
-            'build-apps/how-to-guides/submit-form-data',
             'build-apps/how-to-guides/Setup-Server-side-Pagination-on-List',
             'build-apps/how-to-guides/create-drill-down-view',
             'build-apps/how-to-guides/Create-Nested-Lists',
