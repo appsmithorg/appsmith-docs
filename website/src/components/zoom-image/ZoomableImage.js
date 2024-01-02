5ab6edaf6b839e8f8746f862d008270ab5685da9
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2024-01-02 17:55:34 +0530

    updates to image and hover text

--- website/src/components/zoom-image/ZoomableImage.js
+++ website/src/components/zoom-image/ZoomableImage.js
@@ -1,9 +1,11 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './ZoomableImage.css';
 
 const ZoomableImage = ({ src, alt, caption }) => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
     const tooltipRef = useRef(null);
+    const imageRef = useRef(null);
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -16,25 +18,54 @@ const ZoomableImage = ({ src, alt, caption }) => {
     const handleMouseMove = (e) => {
         const x = e.clientX;
         const y = e.clientY;
-        tooltipRef.current.style.top = y + 20 + 'px';
-        tooltipRef.current.style.left = x + 20 + 'px';
+        setTooltipPosition({ x, y });
     };
+
+    const handleHover = () => {
+        if (imageRef.current) {
+            imageRef.current.style.cursor = 'zoom-in';
+        }
+    };
+
+    const handleHoverOut = () => {
+        if (imageRef.current) {
+            imageRef.current.style.cursor = 'pointer';
+        }
+    };
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsModalOpen(false);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (tooltipRef.current) {
+            tooltipRef.current.style.top = tooltipPosition.y + 20 + 'px';
+            tooltipRef.current.style.left = tooltipPosition.x + 20 + 'px';
+        }
+    }, [tooltipPosition]);
 
     return (
         <div>
             <figure
-                className="zoomable-image image-tooltip"
+                className="zoomable-image"
                 onClick={openModal}
                 onMouseMove={handleMouseMove}
+                onMouseEnter={handleHover}
+                onMouseLeave={handleHoverOut}
             >
-                <img src={src} alt={alt} />
+                <img src={src} alt={alt} ref={imageRef} />
                 <figcaption align="center">
                     <i>{caption}</i>
-                    <span
-                        id="image-tooltip-span"
-                        ref={tooltipRef}
-                    >
-                        Click to expand
+                    <span className="image-tooltip" ref={tooltipRef}>
+                        <i className="fas fa-search-plus"></i>
                     </span>
                 </figcaption>
             </figure>
@@ -43,6 +74,9 @@ const ZoomableImage = ({ src, alt, caption }) => {
                 <div className="zoomable-modal-overlay" onClick={closeModal}>
                     <div className="zoomable-modal-content">
                         <img src={src} alt={alt} />
+                        <figcaption align="center">
+                            <i>{caption}</i>
+                        </figcaption>
                     </div>
                 </div>
             )}
