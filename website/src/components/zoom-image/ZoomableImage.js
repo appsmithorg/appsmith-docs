0588f55125c9f6e932420a517a605984512f0311
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2024-01-02 15:21:28 +0530

    fix: Ability to expand images
    - added an image component
    - added styles for an image component and zooming image
    - added to 2 pages for test purposes

---
+++ website/src/components/zoom-image/ZoomableImage.js
@@ -0,0 +1,58 @@
+// ZoomableImage.js
+
+import React, { useState, useRef } from 'react';
+import './ZoomableImage.css';
+
+const ZoomableImage = ({ src, alt, caption }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const tooltipRef = useRef(null);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const handleMouseMove = (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        tooltipRef.current.style.top = y + 20 + 'px';
+        tooltipRef.current.style.left = x + 20 + 'px';
+    };
+
+    return (
+        <div>
+            <figure
+                className="zoomable-image image-tooltip"
+                onClick={openModal}
+                onMouseMove={handleMouseMove}
+            >
+                <img src={src} alt={alt} />
+                <figcaption align="center">
+                    <i>{caption}</i>
+                    <span
+                        id="image-tooltip-span"
+                        ref={tooltipRef}
+                    >
+                        Click to expand
+                    </span>
+                </figcaption>
+            </figure>
+
+            {isModalOpen && (
+                <div className="zoomable-modal-overlay" onClick={closeModal}>
+                    <div className="zoomable-modal-content">
+                        <img src={src} alt={alt} />
+                        <figcaption align="center">
+                            <i>{caption}</i>
+                        </figcaption>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default ZoomableImage;
