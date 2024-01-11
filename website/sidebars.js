f20a67a42f9b5eea1a9c36a1f45f4af5148b3609
rashmi-sahoo-git <144310336+rashmi-sahoo-git@users.noreply.github.com>
2024-01-11 16:01:35 +0530

    Update sidebars.js
    Fixed comma

--- website/sidebars.js
+++ website/sidebars.js
@@ -398,7 +398,7 @@ const sidebars = {
             `core-concepts/writing-code/workflows`,
             'connect-data/how-to-guides/send-emails-using-the-SMTP-plugin',
             `build-apps/how-to-guides/display-select-options-dynamically`,
-            'build-apps/how-to-guides/add-remove-inputs-in-list'
+            'build-apps/how-to-guides/add-remove-inputs-in-list',
             `build-apps/how-to-guides/navigate-between-pages`,
             'build-apps/how-to-guides/Create-Custom-Widgets-Using-Iframe',
             'build-apps/how-to-guides/Communicate-Between-an-App-and-Iframe',
