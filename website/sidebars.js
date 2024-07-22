af7e4d528ba95b6a10993e5e3cdf30d041d605c7
harshilp24 <harshilpatel9912@gmail.com>
2024-07-22 12:08:16 +0530

    ..

--- website/sidebars.js
+++ website/sidebars.js
@@ -123,7 +123,6 @@ const sidebars = {
                         'getting-started/setup/instance-configuration/authentication/security-assertion-markup-language-saml/auth0',
                         'getting-started/setup/instance-configuration/authentication/security-assertion-markup-language-saml/okta',
                         'getting-started/setup/instance-configuration/authentication/security-assertion-markup-language-saml/ping-identity',
-                         'getting-started/setup/instance-configuration/authentication/security-assertion-markup-language-saml/faqs',
                       ],
                      
                     },
