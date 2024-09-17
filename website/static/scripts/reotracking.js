ccf35f875dc882bac45532705e7e20b1bb0c8d15
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2024-09-17 17:29:33 +0530

    chore: add reo tracking (#2542)
    ## Description 
    
    Provide a concise summary of the changes made in this pull request
    - Add tracking for reo
    
    ## Pull request type
    
    Check the appropriate box:
    
    - [ ] Review Fixes
    - [ ] Documentation Overhaul
    - [x] Feature/Story
        - Link one or more Engineering Tickets
            * 
    - [ ] A-Force
    - [ ] Error in documentation
    - [ ] Maintenance
    
    ## Documentation tickets
    
     Link to one or more documentation tickets:
     - 
    
    ## Checklist
    
    From the below options, select the ones that are applicable:
    
    - [ ] Checked for Grammarly suggestions.
    - [ ] Adhered to the writing checklist.
    - [ ] Adhered to the media checklist.
    - [ ] Verified and updated cross-references or added redirect rules.
    - [ ] Tested the redirect rules on deploy preview.
    - [ ] Validated the modifications made to the content on the deploy
    preview.
    - [ ] Validated the CSS modifications on different screen sizes.

---
+++ website/static/scripts/reotracking.js
@@ -0,0 +1,1 @@
+(!function () { var e, t, n; e = "053d80687ea3965", t = function () { Reo.init({ clientID: "053d80687ea3965" }) }, (n = document.createElement("script")).src = "https://static.reo.dev/" + e + "/reo.js", n.async = !0, n.onload = t, document.head.appendChild(n) })()
