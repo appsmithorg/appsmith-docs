301a6ff38eb3984e65f76cb7ec009876a132b946
Nikhila Jain <99252011+jnikhila@users.noreply.github.com>
2024-09-25 12:50:38 +0530

    fix: reo code (#2552)
    ## Description 
    
    Provide a concise summary of the changes made in this pull request
    - 
    
    ## Pull request type
    
    Check the appropriate box:
    
    - [ ] Review Fixes
    - [ ] Documentation Overhaul
    - [x] Feature/Story
        - Link one or more Engineering Tickets
            * 
    - [ ] A-Force
    - [ ] Error in documentation
    - [ ] Maintenance
    
    ## Documentation tickets
    
     Link to one or more documentation tickets:
     - 
    
    ## Checklist
    
    From the below options, select the ones that are applicable:
    
    - [ ] Checked for Grammarly suggestions.
    - [ ] Adhered to the writing checklist.
    - [ ] Adhered to the media checklist.
    - [ ] Verified and updated cross-references or added redirect rules.
    - [ ] Tested the redirect rules on deploy preview.
    - [ ] Validated the modifications made to the content on the deploy
    preview.
    - [ ] Validated the CSS modifications on different screen sizes.

--- website/static/scripts/reotracking.js
+++ website/static/scripts/reotracking.js
@@ -1,1 +1,12 @@
-(!function () { var e, t, n; e = "053d80687ea3965", t = function () { Reo.init({ clientID: "053d80687ea3965" }) }, (n = document.createElement("script")).src = "https://static.reo.dev/" + e + "/reo.js", n.async = !0, n.onload = t, document.head.appendChild(n) })()
+(function () {
+      var e, t, n;
+      e = "053d80687ea3965";
+      t = function () {
+            Reo.init({ clientID: e });
+      };
+      n = document.createElement("script");
+      n.src = "https://static.reo.dev/" + e + "/reo.js";
+      n.async = true;
+      n.onload = t;
+      document.head.appendChild(n);
+})();
