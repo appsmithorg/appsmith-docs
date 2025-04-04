47bbb1d1e94c893585e68b154972e62f69b78e83
Harshil Patel <43263619+harshilp24@users.noreply.github.com>
2025-04-04 12:28:13 +0530

    feat: Slack (#2830)
    ## Description 
    
    Provide a concise summary of the changes made in this pull request
    - 
    
    ## Pull request type
    
    Check the appropriate box:
    
    - [ ] Review Fixes
    - [ ] Documentation Overhaul
    - [ ] Feature/Story
        - Link one or more Engineering Tickets
            * 
    - [ ] A-Force
    - [ ] Error in documentation
    - [ ] Maintenance
    
    ## Documentation tickets
    
     Link to one or more documentation tickets:
     - 
    
    ## Checklist
    
    From the below options, select the ones that are applicable:
    
    - [ ] Checked for Grammarly suggestions.
    - [ ] Adhered to the writing checklist.
    - [ ] Adhered to the media checklist.
    - [ ] Verified and updated cross-references or added redirect rules.
    - [ ] Tested the redirect rules on deploy preview.
    - [ ] Validated the modifications made to the content on the deploy
    preview.
    - [ ] Validated the CSS modifications on different screen sizes.

--- website/sidebars.js
+++ website/sidebars.js
@@ -324,7 +324,9 @@ const sidebars = {
                     'connect-data/reference/aws-lambda',
                     'connect-data/reference/querying-google-sheets',
                     'connect-data/reference/hubspot',
+                
                     'connect-data/reference/jira',
+                     'connect-data/reference/slack',
                     'connect-data/reference/twilio'
                   ],
                 },
