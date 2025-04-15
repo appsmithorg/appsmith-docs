b8933d8eeebec90e46b3226468f42591d34721cd
Harshil Patel <43263619+harshilp24@users.noreply.github.com>
2025-04-15 10:54:00 +0530

    feat: Version Control and Tagging (Packages) (#2842)
    ## Description 
    
    Provide a concise summary of the changes made in this pull request
    - 
    
    ## Pull request type
    
    Check the appropriate box:
    
    - [ ] Review Fixes
    - [ ] Documentation Overhaul
    - [ ] Feature/Story
        - Link one or more Engineering Tickets
            * 
    - [ ] A-Force
    - [ ] Error in documentation
    - [ ] Maintenance
    
    ## Documentation tickets
    
     Link to one or more documentation tickets:
     - 
    
    ## Checklist
    
    From the below options, select the ones that are applicable:
    
    - [ ] Checked for Grammarly suggestions.
    - [ ] Adhered to the writing checklist.
    - [ ] Adhered to the media checklist.
    - [ ] Verified and updated cross-references or added redirect rules.
    - [ ] Tested the redirect rules on deploy preview.
    - [ ] Validated the modifications made to the content on the deploy
    preview.
    - [ ] Validated the CSS modifications on different screen sizes.

--- website/sidebars.js
+++ website/sidebars.js
@@ -764,7 +764,8 @@ const sidebars = {
             label: 'Reference',
             items: [
               'packages/reference/package',
-              'packages/reference/query-module'
+              'packages/reference/query-module',
+              'packages/reference/versioning'
             ],
           }
         ]
