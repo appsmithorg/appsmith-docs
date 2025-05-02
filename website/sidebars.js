cf9daaad58d6328d44adfa2c290af24d0a0b4a0f
Harshil Patel <43263619+harshilp24@users.noreply.github.com>
2025-05-02 10:24:51 +0530

    docs: update sidebar to include gmail-test-2

--- website/sidebars.js
+++ website/sidebars.js
@@ -323,6 +323,7 @@ const sidebars = {
                     'connect-data/reference/airtable',
                     'connect-data/reference/aws-lambda',
                     'connect-data/reference/querying-google-sheets',
+                     'connect-data/reference/gmail-test2',
                     'connect-data/reference/hubspot',
                     'connect-data/reference/jira',
                      'connect-data/reference/slack',
