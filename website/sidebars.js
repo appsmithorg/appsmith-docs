9896d501a779d521e38034f0dfd64138c769143c
Harshil Patel <43263619+harshilp24@users.noreply.github.com>
2025-05-02 10:08:41 +0530

    docs: update sidebar to include google-drive-test3

--- website/sidebars.js
+++ website/sidebars.js
@@ -325,6 +325,7 @@ const sidebars = {
                     'connect-data/reference/querying-google-sheets',
                     'connect-data/reference/hubspot',
                     'connect-data/reference/jira',
+                     'connect-data/reference/google-docs',
                      'connect-data/reference/slack',
                     'connect-data/reference/twilio',
                     'connect-data/reference/zendesk',
